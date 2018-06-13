f65a18e8306fd25f96cd81e2b279c6fd12b271da
Tuấn Anh <anh.pt@globalflow.com.vn>
2018-06-14 00:01:46 +0700

    TuanAnh

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -21,6 +21,16 @@ class ToolServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/database' => public_path('/exels')
         ], 'seed');
+
+        $this->publishes([
+            __DIR__.'/config/tool.php' => config_path('tool.php'),
+        ], 'config');
+    }
+
+    public function register(){
+        $this->app->bind('Tool', function(){
+            return $this->app->make('GFL\Tool\Tool');
+        });
     }
 
 }
